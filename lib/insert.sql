8cfa2f5fd521100d60a4c1ff7b3d42a78a842a5d
James Macapagal <j.macapagal@gmail.com>
2022-06-30 21:00:26 -0500

    commit tree f36edfea3a6fef8307c0dcc52769964d6fcff6ef

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',10,'M','Chocolate','Nice',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',5,'M','Black','Rowdy',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',1,'F','Gray','Meh',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',3,'M','Chocolate','Mean',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',11,'F','Chocolate','Bitch',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',7,'M','Green','Asshole',0);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy',1,'M','Blue','Ok',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,1,'M','Polar','Cool',1);
